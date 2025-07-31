925609c0d9e99cde025279d94804a270881e71f0
Zandermelon <Zandermelon428@gmail.com>
2025-07-31 02:12:48 -0700

    added update function

--- task.py
+++ task.py
@@ -3,13 +3,17 @@ class Task:
         self.description = description
         self.progress = "todo"
 
+    def update(self, update):
+        self.description = update
+
     def markInProgress(self):
         self.progress = "in-progress"
 
     def markDone(self):
-        self.progress = "Done"
+        self.progress = "done"
 
     def getDescription(self):
         return self.description
     def getProgress(self):
         return self.progress
+    
